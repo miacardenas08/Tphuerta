e87905a01b673c0d6d2ec15f99b529455692df41
miacardenas08 <136343840+miacardenas08@users.noreply.github.com>
2025-11-18 18:32:50 -0300

    Update undato.js

--- undato.js
+++ undato.js
@@ -5,7 +5,7 @@ import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/12
 const firebaseConfig = {
     apiKey: "AIzaSyDllEU7AG4Alvu8pPADt-TSlFMR5clNBj8",
     authDomain: "proyectogrupohuerta-9e970.firebaseapp.com",
-    databaseURL: "p0",
+    databaseURL: "https://proyectogrupohuerta-9e970-default-rtdb.firebaseio.com",
     projectId: "proyectogrupohuerta-9e970",
     storageBucket: "proyectogrupohuerta-9e970.firebasestorage.app",
     messagingSenderId: "104414144386",
